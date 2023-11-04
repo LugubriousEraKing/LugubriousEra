ffb62f384afd22dae5404045426f1af59eb06a7c
LugubriousEraKing <149725075+LugubriousEraKing@users.noreply.github.com>
2023-11-04 05:59:59 -0500

    Update Login.js

--- Netlify/Functions/Login.js
+++ Netlify/Functions/Login.js
@@ -1,24 +1,40 @@
-// netlify/functions/login.js
+const fetch = require('node-fetch');
+const storeUserDataUrl = 'storeuserdata.js';
 
 exports.handler = async (event, context) => {
   try {
-    // Parse the user login data from the request body.
     const { email, password } = JSON.parse(event.body);
 
     // Perform user login logic here, e.g., with Netlify Identity.
     // - Authenticate the user using the provided email and password.
     // - Handle login errors, such as incorrect credentials.
 
-    // Return a success message and user data upon successful login.
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Login successful!", userData: {} }),
-    };
+    // Assuming you have successfully authenticated the user
+    const user = event.clientContext.user;
+
+    // Create a user document in FaunaDB
+    const response = await fetch(storeUserDataUrl, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${user.token.access_token}`,
+      },
+    });
+
+    if (response.ok) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Login successful!', userData: {} }),
+      };
+    } else {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Error creating user document in FaunaDB' }),
+      };
+    }
   } catch (error) {
-    // Handle login errors and return an appropriate response.
     return {
       statusCode: 400,
-      body: JSON.stringify({ error: "Login failed. Please check your credentials." }),
+      body: JSON.stringify({ error: 'Login failed. Please check your credentials.' }),
     };
   }
 };
