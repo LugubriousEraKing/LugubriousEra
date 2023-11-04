9aa52067cc02b4330f2e9f20825553bb11a08de5
LugubriousEraKing <149725075+LugubriousEraKing@users.noreply.github.com>
2023-11-04 00:57:02 -0500

    script.js

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-document.getElementById('registration-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('registration-email').value;
-    const password = document.getElementById('registration-password').value;
-    
-    netlifyIdentity.signup(email, password, (user) => {
-        // Handle successful registration
-        const message = document.getElementById('registration-message');
-        message.textContent = 'Registration success: ' + user.email;
-        message.style.color = 'green';
-        // Redirect to the registration success page
-        window.location.href = 'registration_success.html';
-    }).catch((error) => {
-        // Handle registration failure
-        const message = document.getElementById('registration-message');
-        message.textContent = 'Registration failed: ' + error;
-        message.style.color = 'red';
+// JavaScript code for your LugubriousEra web app
+
+// Initialize Netlify Identity
+netlifyIdentity.init();
+
+// Function to handle a successful registration
+function registrationSuccess(user) {
+    alert('Registration successful!'); // Display a success message
+    // You can redirect to a success page or add more functionality here
+}
+
+// Function to handle a successful login
+function loginSuccess(user) {
+    alert('Login successful!'); // Display a success message
+    // You can redirect to a success page or add more functionality here
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('login-form');
+    const registrationForm = document.getElementById('registration-form');
+    const forgotPasswordButton = document.getElementById('forgot-password-button');
+
+    // Add an event listener for the login form
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const email = document.getElementById('login-email').value;
+        const password = document.getElementById('login-password').value;
+        netlifyIdentity.login(email, password)
+            .then(user => loginSuccess(user)) // Handle successful login
+            .catch(error => alert('Login failed: ' + error)); // Handle login failure
     });
-});
 
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('login-email').value;
-    const password = document.getElementById('login-password').value;
+    // Add an event listener for the registration form
+    registrationForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const email = document.getElementById('registration-email').value;
+        const password = document.getElementById('registration-password').value;
+        netlifyIdentity.signup(email, password)
+            .then(user => registrationSuccess(user)) // Handle successful registration
+            .catch(error => alert('Registration failed: ' + error)); // Handle registration failure
+    });
 
-    netlifyIdentity.login(email, password, (user) => {
-        // Handle successful login
-        const message = document.getElementById('login-message');
-        message.textContent = 'Login success: ' + user.email;
-        message.style.color = 'green';
-        // Redirect to the login success page
-        window.location.href = 'success.html';
-    }).catch((error) => {
-        // Handle login failure
-        const message = document.getElementById('login-message');
-        message.textContent = 'Login failed: ' + error;
-        message.style.color = 'red';
+    // Add an event listener for the "Forgot Your Password" button
+    forgotPasswordButton.addEventListener('click', function () {
+        netlifyIdentity.open('recover'); // Open the password recovery modal
     });
 });
