9a02f35305796a1122a9cd2fe985043cb324cdee
LugubriousEraKing <149725075+LugubriousEraKing@users.noreply.github.com>
2023-11-04 22:13:44 -0500

    Update getinventory.js

--- Netlify/Functions/getinventory.js
+++ Netlify/Functions/getinventory.js
@@ -1,13 +1,11 @@
 // netlify/functions/getInventory.js
 
 exports.handler = async (event, context) => {
-  // Simulate player inventory data (replace with actual data retrieval logic).
-  const playerInventory = {
-    items: [
-      { name: "Item 1", quantity: 5 },
-      { name: "Item 2", quantity: 3 },
-    ],
-  };
+  // Simulate player inventory data with item IDs and quantities (replace with actual data retrieval logic).
+  const playerInventory = [
+    { itemId: 1, quantity: 5 },
+    { itemId: 2, quantity: 3 },
+  ];
 
   return {
     statusCode: 200,
