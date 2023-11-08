c13c0971962a1e955f5c2645294ea0365702a99d
LugubriousEraKing <149725075+LugubriousEraKing@users.noreply.github.com>
2023-11-08 00:52:38 -0600

    Update Login.js

--- Netlify/Functions/Login.js
+++ Netlify/Functions/Login.js
@@ -1,41 +1,20 @@
-const fetch = require('node-fetch');
-const storeUserDataUrl = '/.netlify/functions/storeuserdata';
+// login.js
 
-exports.handler = async (event, context) => {
-  try {
-    const { email, password } = JSON.parse(event.body);
+// Check if Netlify Identity library is available
+const netlifyIdentity = window.netlifyIdentity;
+if (netlifyIdentity) {
+    // Add login logic and error handling here
+    // ...
 
-    // Perform user login logic here, e.g., with Netlify Identity.
-    // - Authenticate the user using the provided email and password.
-    // - Handle login errors, such as incorrect credentials.
-
-    // Assuming you have successfully authenticated the user
-    const user = event.clientContext.user;
-
-    // Make a POST request to storeuserdata function
-    const response = await fetch(storeUserDataUrl, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${user.token.access_token}`,
-      },
+    // Handle login status changes
+    netlifyIdentity.on('login', (user) => {
+        // Redirect to the inventory page upon successful login
+        window.location.href = '/inventory.html';
     });
 
-    if (response.ok) {
-      // Redirect to the inventory page upon successful login
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Login successful!', redirectTo: '/inventory.html' }),
-      };
-    } else {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: 'Error creating user document in FaunaDB' }),
-      };
-    }
-  } catch (error) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'Login failed. Please check your credentials.' }),
-    };
-  }
-};
+    netlifyIdentity.on('logout', () => {
+        // Handle logout, if needed.
+    });
+} else {
+    console.error('Netlify Identity library not found. Ensure it is properly included.');
+}
